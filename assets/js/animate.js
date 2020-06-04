137157ef9805f7bb0b84d975a9108825efb9c187
NightingaleMedia.AlbertMacbook <alsigman@gmail.com>
2020-06-04 14:12:23 -0400

    changed colors

--- assets/js/animate.js
+++ assets/js/animate.js
@@ -2,6 +2,7 @@ const menuSection = document.querySelectorAll('section')
 const sectionHeader = document.querySelectorAll('.section--header')
 const menuHeader = document.querySelector('.menu--header')
 const orderButton = document.querySelector('.order-now')
+const locWrap = document.querySelector('.location-holder')
 const blueash = document.querySelector('.blueash')
 const oakley = document.querySelector('.oakley')
 let root = document.documentElement;
@@ -16,6 +17,7 @@ function setHeaderHeight(){
         let hasOpen = [...menuSection].find(section => section.classList.contains('open'))
         if(hasOpen == undefined){ 
         menuHeader.style.height = `${window.innerHeight - menuHeight}px`;
+
         menuHeader.classList.remove('menu-collapsed');
         }
 }
@@ -43,7 +45,7 @@ function getOpenSection() {
 }
 
 const addMiniMenu = () => {
-    let result = ((window.innerHeight - (menuHeader.offsetHeight + getOpenSection().offsetHeight)) / menuSection.length) + 10 ;
+    let result = ((window.innerHeight - (menuHeader.offsetHeight + getOpenSection().offsetHeight + locWrap.offsetHeight)) / menuSection.length) + 10 ;
     (result < 25) ? result = 30 : result;
     root.style.setProperty(`--mini-menu-size`, result + 'px');
 }
