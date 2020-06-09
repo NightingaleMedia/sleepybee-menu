f5280c07c260bf6b9d1db0fc861fa355628737b3
NightingaleMedia.AlbertMacbook <alsigman@gmail.com>
2020-06-09 10:13:52 -0400

    fixed the parse function

--- assets/js/parse.js
+++ assets/js/parse.js
@@ -4,9 +4,9 @@ import * as fetch from './fetch.js'
 const execute = (n) => {
     let url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRyu85xIQj8N-3tG6uDkql7Azd9CKxta9YkG_PLgl5r1WAgUkMrTFO2WQfzL3sPqb0k2GcOMfs7L-QF/pub?output=csv'
     let idObj = {
-        '1' : '02',
-        '2': '4929117772',
-        '3': '14346032192'
+        '1' : '0',
+        '2': '492911777',
+        '3': '1434603219'
     }
     return d3.dsv(',', url + '&' + `gid=${idObj[n]}`, function (d) {
             return {
