73436ea55a0282a4efd2d2ba0fecb1c2c1410190
NightingaleMedia.AlbertMacbook <alsigman@gmail.com>
2020-08-05 23:33:42 -0400

    added php support

--- assets/js/fetch.js
+++ assets/js/fetch.js
@@ -30,7 +30,7 @@ function build() {
   const section = document.querySelectorAll('.section--menucontent')
 //   section.forEach(sect => buildMenuItems(items, sect))
   for(let i =  1;  i <= section.length; i++){
-        parse.executeSheets(i)
+        parse.executePHP(i)
         
         .then(result => buildMenuItems(result, section[i-1]))
         .catch(err => { 
